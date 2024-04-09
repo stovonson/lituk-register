786bfd5e487878403507c9f2b7e5fa9b0587a0ab
Rodrick <other@deadinsideartist.art>
2024-04-08 20:13:19 -0500

    Fixing proxy handling

--- dnsconfig.js
+++ dnsconfig.js
@@ -3,6 +3,11 @@
 var reg_none = NewRegistrar("none");
 var provider = DnsProvider(NewDnsProvider("cloudflare"));
 
+var proxy = {
+  "on": { "cloudflare_proxy": "on" },
+  "off": { "cloudflare_proxy": "off" }
+}
+
 // -- Functions -- //
 
 function getDomains(filepath) {
@@ -25,23 +30,29 @@ for (var domain in domains) {
 	var info = domains[domain];
 	list[info.subdomain] = [];
 	
+	// Proxy
+	var proxied = proxy.on;
+	if (info.proxied === false) {
+		proxied = proxy.off;
+	}
+	
 	// A Records
 	if (info.record.A) {
 		for (var i in info.record.A) {
-			list[info.subdomain].push(A(info.subdomain, IP(info.record.A[i]), info.proxied));
+			list[info.subdomain].push(A(info.subdomain, IP(info.record.A[i]), proxied));
 		}
 	}
 	
 	// AAA Records
 	if (info.record.AAA) {
 		for (var i in info.record.AAA) {
-			list[info.subdomain].push(AAA(info.subdomain, info.record.AAA[i], info.proxied));
+			list[info.subdomain].push(AAA(info.subdomain, info.record.AAA[i], proxied));
 		}
 	}
 	
 	// CNAME Records
 	if (info.record.CNAME) {	
-		list[info.subdomain].push(CNAME(info.subdomain, info.record.CNAME, info.proxied));
+		list[info.subdomain].push(CNAME(info.subdomain, info.record.CNAME, proxied));
 	}
 	
 	// MX Records
