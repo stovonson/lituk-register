726e714112ab0a21f521e82f408365ffb2ac9829
Rodrick <other@deadinsideartist.art>
2024-04-10 09:48:06 -0500

    Update dnsconfig.js

--- dnsconfig.js
+++ dnsconfig.js
@@ -2,11 +2,11 @@
 
 var reg_none = NewRegistrar("none");
 var provider = DnsProvider(NewDnsProvider("cloudflare"));
-
 var proxy = {
   "on": { "cloudflare_proxy": "on" },
   "off": { "cloudflare_proxy": "off" }
 }
+var base = "sillybilly.club";
 
 // -- Functions -- //
 
@@ -28,7 +28,7 @@ var list = {};
 
 for (var domain in domains) {
 	var info = domains[domain];
-	list[info.subdomain] = [];
+	list[base] = [];
 	
 	// Proxy
 	var proxied = proxy.on;
@@ -39,40 +39,40 @@ for (var domain in domains) {
 	// A Records
 	if (info.record.A) {
 		for (var i in info.record.A) {
-			list[info.subdomain].push(A(info.subdomain, IP(info.record.A[i]), proxied));
+			list[base].push(A(info.subdomain, IP(info.record.A[i]), proxied));
 		}
 	}
 	
 	// AAA Records
 	if (info.record.AAA) {
 		for (var i in info.record.AAA) {
-			list[info.subdomain].push(AAA(info.subdomain, info.record.AAA[i], proxied));
+			list[base].push(AAA(info.subdomain, info.record.AAA[i], proxied));
 		}
 	}
 	
 	// CNAME Records
 	if (info.record.CNAME) {	
-		list[info.subdomain].push(CNAME(info.subdomain, info.record.CNAME, proxied));
+		list[base].push(CNAME(info.subdomain, info.record.CNAME, proxied));
 	}
 	
 	// MX Records
 	if (info.record.MX) {
 		for (var i in info.record.MX) {
-			list[info.subdomain].push(MX(info.subdomain, 10, info.record.MX[i]));
+			list[base].push(MX(info.subdomain, 10, info.record.MX[i]));
 		}
 	}
 	
 	// NS Records
 	if (info.record.NS) {
 		for (var i in info.record.NS) {
-			list[info.subdomain].push(NS(info.subdomain, info.record.NS[i]));
+			list[base].push(NS(info.subdomain, info.record.NS[i]));
 		}
 	}
 	
 	// A Records
 	if (info.record.TXT) {
 		for (var i in info.record.TXT) {
-			list[info.subdomain].push(TXT(info.subdomain, info.record.TXT[i]));
+			list[base].push(TXT(info.subdomain, info.record.TXT[i]));
 		}
 	}
 }
